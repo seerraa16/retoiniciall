5e9966ae374e85f1e86d0d0a86badac1dcd0137f
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-09-17 20:26:08 +0200

    definir movimientos del caballo

---
+++ numeros.py
@@ -0,0 +1,16 @@
+def movimientos_caballo(x, y):
+    movimientos = [(-2, -1), (-1, -2), (-2, 1), (-1, 2),
+                   (1, -2), (2, -1), (1, 2), (2, 1)]
+    contador = 0
+
+    for dx, dy in movimientos:
+        nuevo_x = x + dx
+        nuevo_y = y + dy
+        if 1 <= nuevo_x <= 3 and 1 <= nuevo_y <= 3:
+            contador += 1
+
+    return contador
+
+
+
+
